3e192e53ac569f4d5dc0519f4344ad8491702aa1
RobustProgram <robustprogram@gmail.com>
2017-04-28 10:38:21 +1000

    Initial Files

---
+++ src/MainClass.java
@@ -0,0 +1,16 @@
+/**
+ * Main Class for everything
+ * @author Enter zIDs
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
