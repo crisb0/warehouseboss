8787a0a4a460b83a9f24b00ff1a5082aef36d406
z3460229 <1996shyam@gmail.com>
2017-05-10 17:10:52 +1000

    Create Template.java

---
+++ src/Template.java
@@ -0,0 +1,59 @@
+
+public class Template {
+	private int[][] t;
+	private Block[][] b;
+	
+	public Template(int i){
+		if(i == 1){
+			template1();
+		} else if (i == 2){
+			template2();
+		} else {
+			template3();
+		}
+	}
+	
+	public void template1(){
+		int[][] temp = new int[3][2];
+		this.t = temp;
+		b = new Block[3][2];
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 2; j++){
+				b[i][j] = new Block(0);
+			}
+		}
+	}
+	
+	public void template2(){
+		int[][] temp = new int[2][2];
+		this.t = temp;
+		b = new Block[3][2];
+		for(int i = 0; i < 2; i++){
+			for(int j = 0; j < 2; j++){
+				b[i][j] = new Block(0);
+			}
+		}
+		
+	}
+	
+	public void template3(){
+		int[][] temp = new int[3][3];
+		temp[1][1] = 1;
+		this.t = temp;
+		b = new Block[3][3];
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				b[i][j] = new Block(0);
+			}
+		}
+		b[1][1] = new Block(1);
+	}
+	
+	public int[][] getTemp(){
+		return this.t;
+	}
+	
+	public Block[][] getBlockTemp(){
+		return this.b;
+	}
+}
