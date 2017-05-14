44def7b8b513e6ea07950d6a66ca116f90ca3e5b
Chris Bui <christine.tvbui@gmail.com>
2017-05-15 08:01:52 +1000

    new templates added for testing

--- src/map/Template.java
+++ src/map/Template.java
@@ -82,6 +82,121 @@ public class Template {
 		b[1][2] = new Block(1,1,2);
 	}
 	
+	/*
+	 * 0 0 1
+	 * 1 0 0
+	 * 1 0 0
+	 */
+	public void template5() {
+		int[][] temp = new int[3][3];
+		temp[2][0] = 1;
+		temp[1][0] = 1;
+		temp[2][0] = 1;
+		this.t = temp;
+		b = new Block[3][3];
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				b[i][j] = new Block(0,i,j);
+			}
+		}
+	}
+	
+	/*
+	 * 1 1 0 0
+	 * 0 0 0 0
+	 * 0 0 0 0
+	 * 0 0 0 0
+	 */
+	public void template6() {
+		int[][] temp = new int[4][4];
+		temp[0][0] = 1;
+		temp[1][0] = 1;
+		this.t = temp;
+		b = new Block[4][4];
+		for (int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				b[i][j] = new Block(0,i,j);
+			}
+		}
+	}
+	
+	/*
+	 * 0 0 0 1
+	 * 1 0 0 0
+	 * 1 0 0 0
+	 */
+	public void template7() {
+		int[][] temp = new int[4][3];
+		temp[3][0] = 1;
+		temp[1][1] = 1;
+		temp[2][2] = 1;
+		this.t = temp;
+		b = new Block[4][3];
+		for (int i = 0; i < 4; i++) {
+			for(int j = 0; j < 3; j++) {
+				b[i][j] = new Block(0,i,j);
+			}
+		}
+	}
+	
+	/*
+	 * 0 0 0 0
+	 * 0 0 0 0
+	 * 0 0 0 1 
+	 * 1 0 0 1
+	 */
+	public void template8() {
+		int[][] temp = new int[4][4];
+		temp[3][2] = 1;
+		temp[0][3] = 1;
+		temp[3][3] = 1;
+		this.t = temp;
+		b = new Block[4][4];
+		for (int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				b[i][j] = new Block(0,i,j);
+			}
+		}
+	}
+	
+	/*
+	 * 0 0 0 
+	 * 0 0 0
+	 * 0 0 0
+	 * 1 1 0
+	 */
+	public void template9() {
+		int[][] temp = new int[3][4];
+		temp[0][3] = 1;
+		temp[1][3] = 1;
+		this.t = temp;
+		b = new Block[3][4];
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 4; j++) {
+				b[i][j] = new Block(0,i,j);
+			}
+		}
+	}
+	
+	/*
+	 * 0 0 0 0
+	 * 0 0 0 0
+	 * 0 1 1 0
+	 * 0 0 0 0
+	 */
+	public void template10() {
+		int[][] temp = new int[4][4];
+		temp[1][2] = 1;
+		temp[2][2] = 1;
+		this.t = temp;
+		b = new Block[4][4];
+		for (int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				b[i][j] = new Block(0,i,j);
+			}
+		}
+	}
+	
 	public int[][] getTemp(){
 		return this.t;
 	}
