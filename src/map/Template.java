1dd9fd9521dc0c2b8fc27a9abcc4502e76680a11
charleywong <z5060076@zmail.unsw.edu.com>
2017-05-14 23:08:14 +1000

    cleaned up addTemplate(), changed templates

--- src/map/Template.java
+++ src/map/Template.java
@@ -12,45 +12,57 @@ public class Template {
 			template2();
 		} else if (i == 3){
 			template3();
-		} else {
+		} else if (i == 4){
 			template4();
 		}
 	}
 	
 	public void template1(){
-		int[][] temp = new int[3][2];
+		int[][] temp = new int[4][4];
 		this.t = temp;
-		b = new Block[3][2];
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 2; j++){
+		temp[1][1] = 1;
+		temp[3][3] = 1;
+		b = new Block[4][4];
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 4; j++){
 				b[i][j] = new Block(0, i, j);
 			}
 		}
+		b[1][1] = new Block(1,1,1);
+		b[3][3] = new Block(1,3,3);
 	}
 	
 	public void template2(){
-		int[][] temp = new int[2][2];
+		int[][] temp = new int[4][4];
+		temp[0][1] = 1;
+		temp[0][2] = 1;
+		temp[0][3] = 1;
 		this.t = temp;
-		b = new Block[3][2];
-		for(int i = 0; i < 2; i++){
-			for(int j = 0; j < 2; j++){
+		b = new Block[4][4];
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 4; j++){
 				b[i][j] = new Block(0, i, j);
 			}
 		}
+		b[0][1] = new Block(1, 0, 1);
+		b[0][2] = new Block(1, 0, 2);
+		b[0][3] = new Block(1, 0, 2);
 		
 	}
 	
 	public void template3(){
-		int[][] temp = new int[3][3];
-		temp[1][1] = 1;
+		int[][] temp = new int[4][4];
+		temp[2][1] = 1;
+		temp[2][0] = 1;
 		this.t = temp;
-		b = new Block[3][3];
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
+		b = new Block[4][4];
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 4; j++){
 				b[i][j] = new Block(0, i, j);
 			}
 		}
-		b[1][1] = new Block(1, 1, 1);
+		b[2][1] = new Block(1, 2, 1);
+		b[2][0] = new Block(1, 2, 0);
 	}
 	
 	public void template4() {
@@ -63,21 +75,11 @@ public class Template {
 		for (int i = 0; i < 3; i++) {
 			for(int j = 0; j < 4; j++) {
 				b[i][j] = new Block(0,i,j);
-//				System.out.print(b[i][j].getType());
 			}
-//			System.out.println();
 		}
 		b[0][1] = new Block(1,0,1);
 		b[0][2] = new Block(1,0,2);
 		b[1][2] = new Block(1,1,2);
-//		System.out.println(b[i][j]);
-//		for (int i = 0; i < 3; i++) {
-//			for(int j = 0; j < 4; j++) {
-////				b[i][j] = new Block(0,i,j);
-//				System.out.print(b[i][j].getType());
-//			}
-//			System.out.println();
-//		}
 	}
 	
 	public int[][] getTemp(){
