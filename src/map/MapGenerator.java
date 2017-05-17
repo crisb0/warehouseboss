8b2c12ba320eae10ff05a9aa742df80c3a94d808
z3460229 <1996shyam@gmail.com>
2017-05-17 16:16:20 +1000

    Update MapGenerator.java

--- src/map/MapGenerator.java
+++ src/map/MapGenerator.java
@@ -9,33 +9,25 @@ import entity.Box;
 
 public class MapGenerator {
 	
-	private Block[][] puzzleB = new Block[10][10];
-//	private StartingMap s = new StartingMap();
-//	private ArrayList<Template> ts = new ArrayList<Template>();
-//	private ArrayList<Block> goals = new ArrayList<Block>();
-//	private ArrayList<Block> boxes = new ArrayList<Block>();
+	private static final int DIMENSIONS = 10;
+	private Block[][] puzzle = new Block[DIMENSIONS][DIMENSIONS];
 	private Point playerLocation;
 	private List<Box> boxLocs;
 	private List<Point> goalLocs;
 	
 	public MapGenerator(){
 		CreateMap cm  = new CreateMap();
-		FindSolution fs = new FindSolution(cm.getPuzzleB(), cm.getGoalLocs());
-		this.puzzleB = fs.getPuzzle();
+		FindSolution fs = new FindSolution(cm.getPuzzle(), cm.getGoalLocs());
+		this.puzzle = fs.getPuzzle();
 		this.playerLocation = fs.getPlayerLocation();
 		this.boxLocs = fs.getBoxLocs();
 		this.goalLocs = fs.getGoalLocs();
 	}
-	
-//	public static void main (String[] args) {
-//		MapGenerator m = new MapGenerator();
-//		m.displayPuzzle();
-//	}
-	
+
 	public void displayPuzzle() {
 		for(int i = 0; i < 10; i++){
 			for(int j = 0; j < 10; j++){
-				System.out.print(this.puzzleB[j][i].getType());
+				System.out.print(this.puzzle[j][i].getType());
 			}
 			System.out.println();
 		}
@@ -43,10 +35,10 @@ public class MapGenerator {
 
 	public int[][] getGrid() {
 		int[][] grid = new int[10][10];
-		for(int i = 0; i < this.puzzleB.length; i++){
-			for(int j = 0; j < this.puzzleB[0].length; j++){
-				if(this.puzzleB[i][j].getType() != 9){
-					grid[i][j] = this.puzzleB[i][j].getType();
+		for(int i = 0; i < this.puzzle.length; i++){
+			for(int j = 0; j < this.puzzle[0].length; j++){
+				if(this.puzzle[i][j].getType() != 9){
+					grid[i][j] = this.puzzle[i][j].getType();
 				} else {
 					grid[i][j] = 0;
 				}
@@ -56,18 +48,14 @@ public class MapGenerator {
 	}
 
 	public Point getStartingPlayerLoc() {
-		// TODO Auto-generated method stub
 		return this.playerLocation;
 	}
 
 	public List<Box> getBoxLocs() {
-		// TODO Auto-generated method stub
 		return this.boxLocs;
 	}
 
 	public List<Point> getGoalLocs() {
-		// TODO Auto-generated method stub
 		return this.goalLocs;
 	}
 }
-
